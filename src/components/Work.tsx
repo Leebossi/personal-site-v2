51d66be2327939359326805f64b145cc4eaf299d
Leevi Ossi <73876586+Leebossi@users.noreply.github.com>
2026-02-17 16:45:28 +0200

    added personal projects

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import workData from "../data/work.json";
+import projectsData from "../data/projects.json";
 import i18n from "../i18n";
 
 const Work = () => {
@@ -15,7 +16,8 @@ const Work = () => {
               {t("work.position")} - {job.company}
             </h3>
             <h4>
-              {t(`months.${job.startMonth}`)} {job.startYear} - {t(`months.${job.endMonth}`)} {job.endYear}
+              {t(`months.${job.startMonth}`)} {job.startYear} -{" "}
+              {t(`months.${job.endMonth}`)} {job.endYear}
             </h4>
             <ul>
               {job.responsibilities.map((_resp, index) => (
@@ -26,9 +28,35 @@ const Work = () => {
         ))}
       </div>
 
-      <a href={i18n.language === "en" ? "/cv-en.html" : "/cv-fi.html"} target="_blank" rel="noopener noreferrer">
-        <button className="cv-button">{t("work.viewCV")}</button>
+      <a
+        href={i18n.language === "en" ? "/cv-en.html" : "/cv-fi.html"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="action-button">{t("work.viewCV")}</button>
       </a>
+
+      <h1 className="projects-title">{t("projects.title")}</h1>
+      <div className="container projects">
+        {projectsData.Projects.map((project, index) => (
+          <div key={index} className="card">
+            <h3>{t(`projects.${index + 1}.name`)}</h3>
+            <p>{t(`projects.${index + 1}.description`)}</p>
+            <h3>{t("projects.technologies")}</h3>
+            <ul className="technologies-list">
+              {project.technologies.map((tech, index) => (
+                <li key={index}>{tech}</li>
+              ))}
+            </ul>
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <button className="action-button">{t("projects.viewProject")}</button>
+            </a>
+            <a href={project.github} target="_blank" rel="noopener noreferrer">
+              <button className="action-button">{t("projects.viewGitHub")}</button>
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
