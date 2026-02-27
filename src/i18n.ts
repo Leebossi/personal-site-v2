4cf85f52cd9ea9772626935636396eeeec30b07b
Leevi Ossi <leevi.ossi@hotmail.com>
2026-02-27 17:56:20 +0100

    language support fix

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,12 @@
 import i18n from "i18next";
 import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
-  fallbackLng: "fi"
+i18n.use(Backend).use(initReactI18next).init({
+  fallbackLng: "en",
+  supportedLngs: ["en", "fi"],
+  load: "languageOnly",
+  nonExplicitSupportedLngs: false,
 });
 
 export default i18n;
